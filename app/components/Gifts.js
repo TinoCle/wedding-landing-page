cd05e49340296e876bf8aac92cd07dc6c88756b5
Agustín Clemente <aclemente@wilab.io>
2024-11-03 12:07:42 -0300

    feat(gifts): change texts and image

--- app/components/Gifts.js
+++ app/components/Gifts.js
@@ -3,14 +3,17 @@ import React from 'react';
 const Gifts = () => {
   return (
     <div className="heroContainer">
-      <img src="./gifts.png" alt="Gifts Circle" className="backgroundGifts" />
+      <img src="./gifts-pink.png" alt="Gifts Circle" className="backgroundGifts" />
       <div className="textContainer">
         <h1 className="overlayText text-9xl sm:text-9xl md:text-9xl lg:text-9xl mb-9 text-center">Regalos</h1>
         <h1 className="leftText text-2xl sm:text-2xl md:text-2xl lg:text-2xl mt-9 text-center">
-          🏦 Se aceptan donaciones al siguiente alias! 🏦 
+          Su presencia es nuestro mejor regalo.
+        </h1>
+        <h1 className="leftText text-2xl sm:text-2xl md:text-2xl lg:text-2xl mt-1 text-center">
+          Si prefieren un obsequio virtual, nuestro alias es:
         </h1>
         <h1 className="leftText text-4xl sm:text-4xl md:text-4xl lg:text-4xl mt-4 text-center">
-          alias.bancario.maii
+          boda.mai.tino
         </h1>
       </div>
     </div>
